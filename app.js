92a0816dc98bbec2af82eb7c0763be6de4c1e7a8
Shawn Cannon <scanno16@ford.com>
2022-04-11 09:48:12 -0600

    change code from if chain to switch

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
 const buttons = document.querySelectorAll("span");
 const screen = document.querySelector("#screen");
-const operator = document.querySelectorAll(".operator");
+const operators = document.querySelectorAll(".operator");
+
+const allNumbers = document.querySelectorAll("span:not(.operator)");
+const clear = document.querySelector("#clear")
+
+/*
 const buttonsArray = Array.from(buttons);
 operator[1].innerText = "/"
 operator[2].innerText = "*"
+*/
 
+
+
+allNumbers.forEach(number => {
+    number.addEventListener("click", event => {
+        switch (screen.textContent) {
+            case "ERROR":
+                screen.textContent = "ERROR"
+                break;
+            default:
+                screen.textContent += event.target.textContent
+        }
+    })
+})
+
+operators.forEach(operator => {
+    operator.addEventListener("click", event => {
+        switch (operator.textContent) {
+            case "=":
+                screen.textContent = eval(screen.textContent)
+                break;
+            case "X":
+                screen.textContent += "*"
+                break;
+            case "÷":
+                screen.textContent += "/"
+                break;
+            default:
+                screen.textContent += event.target.textContent
+        }
+    })
+})
+
+
+clear.addEventListener("click", () => {
+    screen.textContent = ""
+})
+
+
+
+
+
+/*
 buttonsArray.map(button => button.addEventListener("click", event => {
 
     if (button.textContent !== "=") {
@@ -18,4 +66,7 @@ buttonsArray.map(button => button.addEventListener("click", event => {
     if (screen.textContent === "Infinity") {
         screen.textContent = "ERROR"
     };
-}))
+    if (screen.textContent[0] === "0") {
+        screen.textContent = "ERROR"
+    }
+}))*/
